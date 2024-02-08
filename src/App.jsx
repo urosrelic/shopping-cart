1f0f72ec024035d62c75efdc7a6c1d9210be275a
urosrelic <urosrelicc@gmail.com>
2024-02-08 16:39:53 +0100

    feat: add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import { Header } from './components/Header';
+import { Products } from './pages/Products';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='products' element={<Products />} />
+          {/* <Route path='jewelry' element={} /> */}
+          {/* <Route path='men' element={} /> */}
+          {/* <Route path='women' element={} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
