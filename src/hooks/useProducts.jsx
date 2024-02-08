f6171f025510051aa728594a65aa5520024b9539
urosrelic <urosrelicc@gmail.com>
2024-02-08 16:26:07 +0100

    feat: add custom hook for fetching data
    Also can be used for filtering data

---
+++ src/hooks/useProducts.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const API_URL = 'https://fakestoreapi.com/products';
+
+export const useProducts = (filter) => {
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error('server error');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const appliedFilterData = filter
+          ? data.filter((item) => item.category.toLowerCase() === filter)
+          : data;
+
+        // remove electronics
+        const filteredData = appliedFilterData.filter(
+          (item) => item.category.toLowerCase() != 'electronics'
+        );
+        setProducts(filteredData);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { products, error, loading };
+};
