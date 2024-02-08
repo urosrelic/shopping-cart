645948cda3833204f31e8489eb7130517d5ace31
urosrelic <urosrelicc@gmail.com>
2024-02-08 16:26:17 +0100

    feat: add products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+import { ProductItem } from '../components/ProductItem';
+import { useProducts } from '../hooks/useProducts';
 export const Products = () => {
-  return <div className='products-container'></div>;
+  const [filter, setFilter] = useState(null);
+  const { products, error, loading } = useProducts(filter);
+
+  return (
+    <div className='products-container'>
+      {products &&
+        products.length > 0 &&
+        products.map((product) => (
+          <ProductItem key={product.id} productData={product} />
+        ))}
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+    </div>
+  );
 };
