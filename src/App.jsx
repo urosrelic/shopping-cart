db6fac62210c81ac38e679040d99249ec2bf4b72
urosrelic <urosrelicc@gmail.com>
2024-02-09 17:16:16 +0100

    fix: git merge errors

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,17 @@ import { Products } from './pages/Products';
 function App() {
   return (
     <div className='App'>
-<<<<<<< HEAD
       <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
-=======
-      <Header />
-      <Router>
+        <Header>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='products' element={<Products />} />
->>>>>>> 1f0f72ec024035d62c75efdc7a6c1d9210be275a
           {/* <Route path='jewelry' element={} /> */}
           {/* <Route path='men' element={} /> */}
           {/* <Route path='women' element={} /> */}
