7fede5c4e81ee29801df33512d06bb603ee11e29
urosrelic <urosrelicc@gmail.com>
2024-02-09 17:48:44 +0100

    feat: add products page
    also added a new font along with some  styling changes

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/prop-types */
 export const ProductItem = ({ productData }) => {
   return (
-    <div className='product'>
-      <h1>{productData.title}</h1>
+    <div className='product-container'>
+      <span id='product-title'>{productData.title}</span>
+      <div className='product-image'>
+        <img src={productData.image} />
+      </div>
+      <div id='add-to-cart-btn'>Add to cart</div>
     </div>
   );
 };
