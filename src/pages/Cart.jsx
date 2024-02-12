67c3c1c23a30c8839052208ac4efb323021fe782
urosrelic <urosrelicc@gmail.com>
2024-02-12 17:26:12 +0100

    chore: add empty cart text

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,9 +5,11 @@ export const Cart = ({ cart }) => {
   return (
     <div className='cart-container'>
       <ul>
-        {cart.map((item) => (
-          <CartItem key={item.id} item={item} />
-        ))}
+        {cart.length > 0 ? (
+          cart.map((item) => <CartItem key={item.id} item={item} />)
+        ) : (
+          <h2>Empty cart</h2>
+        )}
       </ul>
     </div>
   );
